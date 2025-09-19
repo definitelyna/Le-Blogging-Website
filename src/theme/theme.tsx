02af0d61dc003ecb48cea115e2c8268f9565cd6d
definitelyna <nguyentrongnamanh@gmail.com>
2025-09-19 16:38:24 +0700

    theme

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -3,27 +3,22 @@ import { theSeasons } from "../fonts/theSeason";
 
 const theme = createTheme({
   palette: {
+    primary: {
+      main: "#0f4d92",
+      secondary: "#F9F9FA",
+    },
     background: {
       default: "#ffffff",
     },
     text: {
       primary: "#000000",
+      secondary: "#717182",
     },
   },
   typography: {
-    fontFamily: "Arial, sans-serif",
-  },
-  components: {
-    MuiContainer: {
-      styleOverrides: {
-        root: {
-          minHeight: "100vh",
-          minWidth: "100vw",
-          display: "flex",
-          flexDirection: "column",
-        },
-      },
-    },
+    //Set default font family as ui-sans-serif
+    fontFamily:
+      "ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
   },
 });
 
