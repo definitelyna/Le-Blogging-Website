a92fdab71dfaab9efde7b256d2bf39ca70852a1b
definitelyna <nguyentrongnamanh@gmail.com>
2025-09-22 15:54:54 +0700

    Auto redirect to /admin/posts when /admin is accessed

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/admin", // when user visits /admin
+        destination: "/admin/posts", // redirect to subroute
+        permanent: true, // 308 permanent redirect (set false for temporary)
+      },
+    ];
+  },
+};
+
 export default nextConfig;
